e0aeeaab1cce5f254eae95fcba82f2cb2e0d26fa
Kristjan Roosild <kristjan.r@gmail.com>
2015-11-14 16:03:16 +0200

    fix send sms button

--- app/views.py
+++ app/views.py
@@ -1,24 +1,16 @@
 import logging
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 import requests
 
 
 def index(request):
     return render(request, 'app/index.html')
 
-    #
-    # <input type="checkbox">
-    # <input type="hidden" name="username" value="f35583f5f9fcd7a8a13f36a10afca6aa">
-    # <input type="hidden" name="password" value="ea2254154190fab7a6a33c3ec79a21f0">
-    # <input type="hidden" name="text" value="A short Tour description. Accept the Tour: https://intense-tor-6246.herokuapp.com/app/respond">
-    # <input type="hidden" name="to" value="+37253498963">
-
 
 def guides(request):
     return render(request, 'app/guides.html')
+
 
 def send_sms(request):
     post_body = dict(
@@ -31,8 +23,10 @@ def send_sms(request):
     logging.warning('post response: %s', r.content)
     return render(request, 'app/sms_sent.html')
 
+
 def respond(request):
     return render(request, 'app/respond.html')
+
 
 def accepted(request):
     return render(request, 'app/accepted.html')
