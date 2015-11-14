3027741a47feff1af12cd4a819ebf396acb8f126
Kristjan Roosild <kristjan.r@gmail.com>
2015-11-14 15:32:33 +0200

    added tour info input form

--- app/views.py
+++ app/views.py
@@ -16,6 +16,10 @@ def index(request):
     # <input type="hidden" name="text" value="A short Tour description. Accept the Tour: https://intense-tor-6246.herokuapp.com/app/respond">
     # <input type="hidden" name="to" value="+37253498963">
 
+
+def guides(request):
+    return render(request, 'app/guides.html')
+
 def send_sms(request):
     post_body = dict(
         username='f35583f5f9fcd7a8a13f36a10afca6aa',
