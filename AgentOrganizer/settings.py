b8b7626caec02dc4a09f1d4c5bf413f75f4005de
Kristjan Roosild <kristjan.r@gmail.com>
2015-12-05 19:53:06 +0200

    Small tweaks & refactor

--- AgentOrganizer/settings.py
+++ AgentOrganizer/settings.py
@@ -101,9 +101,9 @@ DATABASES['default'] = dj_database_url.config()
 LANGUAGE_CODE = 'en-us'
 # TIME_ZONE = 'Europe/Tallinn'
 USE_I18N = True
-USE_L10N = True
+USE_L10N = False
 USE_TZ = False
-
+TIME_FORMAT = 'H:i'
 
 # Enable Connection Pooling (if desired)
 # DATABASES['default']['ENGINE'] = 'django_postgrespool'
