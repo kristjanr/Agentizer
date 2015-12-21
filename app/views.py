b89d6d08e4b47a57ae82b6c2c12ea633c0068f40
Kristjan Roosild <kristjan.r@gmail.com>
2015-12-21 03:09:23 +0200

    Guide can change his 'no' to 'yes' Translated langing page. Small translation fixes

--- app/views.py
+++ app/views.py
@@ -37,7 +37,7 @@ def md5_hash(s):
 
 def send_emails(context):
     tour_info = get_template('app/tour_content_email.html').render(Context(context))
-    send_mail('Tour details', tour_info, DEFAULT_FROM_EMAIL, [context['guide'].email, context['user'].email], html_message=tour_info)
+    send_mail(_('Tour details'), tour_info, DEFAULT_FROM_EMAIL, [context['guide'].email, context['user'].email], html_message=tour_info)
 
 
 @login_required
@@ -55,7 +55,7 @@ def add_or_edit_tour(request, tour_id=None):
             tour_dict['id'] = get_object_or_404(Tour, id=tour_id).id if tour_id else None
             tour = Tour(**tour_dict)
             tour.save()
-            if request.POST['submit'] == 'Save':
+            if request.POST['submit'] == _('Save'):
                 return redirect('edit_tour', tour.id)
             # redirect to add guides url with form id
             return redirect('add_guides', tour.id)
@@ -202,26 +202,25 @@ def answer(request):
     uid = request.POST['uid']
     guide_tour = GuideTour.objects.get(uid=uid)
 
+    # Check if someone else has already accepted the tour
     for gt in guide_tour.tour.guidetour_set.all():
         if gt != guide_tour and gt.answer:
             return render(request, 'app/accepted.html')
 
-    guide_answer = True if request.POST['answer'] == 'yes' else False
-
+    guide_answer = True if request.POST['answer'] == _('yes') else False
     context = dict(tour=guide_tour.tour, guide=guide_tour.guide, user=guide_tour.tour.user)
 
-    # do not let the add_guides to change their answers
-    if guide_tour.answer is not None:
-        if guide_tour.answer:
-            return render(request, 'app/accepted.html', context=context)
-        else:
-            return render(request, 'app/rejected.html')
+    # Do not allow to change the answer from yes to no
+    if guide_tour.answer:
+        return render(request, 'app/accepted.html', context=context)
 
     guide_tour.answer = guide_answer
     guide_tour.save()
+
+    # The guide answered no
     if not guide_tour.answer:
-        return render(request, 'app/')
+        return render(request, 'app/rejected.html')
 
+    # The guide accepted the Tour
     send_emails(context)
-
     return render(request, 'app/accepted.html', context=context)
