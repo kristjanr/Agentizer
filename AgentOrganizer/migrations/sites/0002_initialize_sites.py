46d8e3ad234b369311ed2b30f80cd0b3fb737399
Kristjan Roosild <kristjan.r@gmail.com>
2016-01-06 22:51:44 +0200

    save some money: removed proximo and switched to agentizer.herokuapp.com

--- AgentOrganizer/migrations/sites/0002_initialize_sites.py
+++ AgentOrganizer/migrations/sites/0002_initialize_sites.py
@@ -11,6 +11,7 @@ def insert_sites(apps, schema_editor):
 
     # Register SITE_ID = 1
     Site.objects.create(id=1, domain='agentizer.com', name='Agentizer.com')
+    Site.objects.create(id=2, domain='agentizer.herokuapp.com', name='agentizer.herokuapp.com')
 
 
 class Migration(migrations.Migration):
